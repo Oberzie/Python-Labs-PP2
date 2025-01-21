3a8126d971a4a286edee89ac58ede5917e4991f1
Sadyr Zhiger <zhigersadyr01@gmail.com>
2025-01-22 01:14:04 +0500

    taks5

--- lab3/classes/task5.py
+++ lab3/classes/task5.py
@@ -1,16 +1,39 @@
-def filter(x):
-    if x < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if x % i == 0:
-            return False
-    return True
+class Account:
+ def __init__(self):
+    self.owner = input()
+    self.balance = int(input())
+ 
+ def deposit(self, amount):
+      if amount > 0:
+            self.balance += amount
+            print(f"Deposit of {amount} completed. New balance: {self.balance}")
+      else:
+            print("Deposit amount must be greater than 0.")
+            
+ def withdraw(self, amount):  
+     if amount > self.balance:
+            print(f"Withdrawal of {amount} declined. Insufficient funds. Available balance: {self.balance}")
+     elif amount <= 0:
+            print("Withdrawal amount must be greater than 0.")
+     else:
+            self.balance -= amount
+            print(f"Withdrawal of {amount} completed. New balance: {self.balance}")
 
-# List of numbers to filter
-numbers = list(map(int, input("Enter a list of numbers separated by spaces: ").split()))
+ 
+ def __str__(self):
+        return f"Account owner: {self.owner}\nAccount balance: {self.balance}"
 
-# Use filter() with a lambda to filter prime numbers
-prime_numbers = list(filter(lambda x: is_prime(x), numbers))
+       
 
-# Output the result
-print("Prime numbers in the list are:", prime_numbers)
+
+account = Account()
+print(account)
+
+account.deposit(500)
+account.deposit(-200)  # Invalid deposit
+
+account.withdraw(300)
+account.withdraw(1500)  # Overdraw attempt
+account.withdraw(-100)  # Invalid withdrawal
+
+print(account)
