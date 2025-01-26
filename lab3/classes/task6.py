18ce22d8fd825ec2eef074cbd085a4306bffb4c4
Sadyr Zhiger <zhigersadyr01@gmail.com>
2025-01-26 22:09:18 +0500

    task6

--- lab3/classes/task6.py
+++ lab3/classes/task6.py
@@ -1,16 +1,10 @@
-def filter(x):
-    if x < 2:
+def filt(n):
+    if n <= 1:  
         return False
-    for i in range(2, int(n**0.5) + 1):
-        if x % i == 0:
+    for i in range(2, int(n ** 0.5) + 1):  
+        if n % i == 0: 
             return False
-    return True
-
-# List of numbers to filter
+    return True  
 numbers = list(map(int, input("Enter a list of numbers separated by spaces: ").split()))
-
-# Use filter() with a lambda to filter prime numbers
-prime_numbers = list(filter(lambda x: is_prime(x), numbers))
-
-# Output the result
+prime_numbers = list(filter(lambda x: filt(x), numbers))
 print("Prime numbers in the list are:", prime_numbers)
